4c375e97d773b28c8cf0a9a0b4e74b3db7dbc0e2
lebron <worthmake307@gmail.com>
2022-08-09 19:08:56 -0700

    first commit

---
+++ db/db.js
@@ -0,0 +1,404 @@
+export default {
+	bio: {
+		about: {
+			text: [
+				"A Full Stack Engineer who has been working as a professional web developer for more than 9+ years, most proficient at front- end development using JavaScript.",
+				"Well-versed in developing web pages using JavaScript and TypeScript program languages. Expert level programming skills in React JS and working with React Flux architecture. Good Expertise in analyzing the Document Object Model (DOM) Layout, DOM Functions, and Java Script functions, Cascading Styles.",
+				"Hands-on Experience in Back-end with Node, Laravel, Spring, Django, ASP.NET and GraphQL.",
+				"Good Knowledge with SQL programming language in MySQL, PostgreSQL, MongoDB and DynamoDB.",
+				"Excellent Client interaction skills and proven experience in working independently as well as in a team."
+			],
+		},
+		contact: {
+			text: [
+				"If you would like to get in touch with me, be it for exploring a technology, a business, or to just say hi, feel free to send me an email. My email address are: blockmasterdev0307@gmail.com, worthmake307@gmail.com",
+			],
+		},
+	},
+	skills: [
+		{
+			title: "Languages",
+			skillName: "JavaScript, C++",
+			color: "1",
+			percentage: "70",
+		},
+		{
+			title: "Frameworks/Libraries",
+			skillName: "Angular, Reactjs, Vue",
+			color: "6",
+			percentage: "50",
+		},
+		{
+			title: "Backend",
+			skillName: "Nodejs, MongoDB",
+			color: "2",
+			percentage: "40",
+		},
+		{
+			title: "Clouds",
+			skillName: "AWS(EC2, S3), Heroku, Netlify",
+			color: "3",
+			percentage: "30",
+		},
+		{
+			title: "Design",
+			skillName: "HTML, Bootstrap, SCSS",
+			color: "4",
+			percentage: "70",
+		},
+		{
+			title: "Version Control",
+			skillName: "GitHub, JIRA, Trello",
+			color: "7",
+			percentage: "70",
+		}
+	],
+	skills1: [
+		{
+			title: "Languages",
+			skillName: "JavaScript",
+			color: "1",
+			percentage: "98",
+		},
+		{
+			title: "Languages",
+			skillName: "TypeScript",
+			color: "1",
+			percentage: "95",
+		},
+		{
+			title: "Languages",
+			skillName: "PHP",
+			color: "1",
+			percentage: "85",
+		},
+		{
+			title: "Languages",
+			skillName: "Python",
+			color: "1",
+			percentage: "90",
+		},
+		{
+			title: "Languages",
+			skillName: "Solidity",
+			color: "1",
+			percentage: "90",
+		},
+		{
+			title: "Languages",
+			skillName: "C#",
+			color: "1",
+			percentage: "85",
+		},
+		{
+			title: "Languages",
+			skillName: "Java",
+			color: "1",
+			percentage: "90",
+		},
+	],
+	skills2: [
+		{
+			title: "Front-end(Frameworks/Libraries)",
+			skillName: "React",
+			color: "2",
+			percentage: "95",
+		},
+		{
+			title: "Front-end(Frameworks/Libraries)",
+			skillName: "Next",
+			color: "2",
+			percentage: "90",
+		},
+		{
+			title: "Front-end(Frameworks/Libraries)",
+			skillName: "Gatsby",
+			color: "2",
+			percentage: "95",
+		},
+		{
+			title: "Front-end(Frameworks/Libraries)",
+			skillName: "Vue",
+			color: "2",
+			percentage: "90",
+		},
+	],
+	skills3: [
+		{
+			title: "Back-end(Frameworks/Platforms)",
+			skillName: "Node/Express",
+			color: "6",
+			percentage: "95",
+		},
+		{
+			title: "Back-end(Frameworks/Platforms)",
+			skillName: "GraphQL",
+			color: "6",
+			percentage: "85",
+		},
+		{
+			title: "Back-end(Frameworks/Platforms)",
+			skillName: "Laravel",
+			color: "6",
+			percentage: "90",
+		},
+		{
+			title: "Back-end(Frameworks/Platforms)",
+			skillName: "Django",
+			color: "6",
+			percentage: "85",
+		},
+	],
+	skills4: [
+		{
+			title: "Database",
+			skillName: "MySQL",
+			color: "4",
+			percentage: "95",
+		},
+		{
+			title: "Database",
+			skillName: "PostgreSQL",
+			color: "4",
+			percentage: "89",
+		},
+		{
+			title: "Database",
+			skillName: "MongoDB",
+			color: "4",
+			percentage: "93",
+		},
+		{
+			title: "Database",
+			skillName: "DynamoDB",
+			color: "4",
+			percentage: "90",
+		},
+	],
+	skills5: [
+		{
+			title: "Version Control",
+			skillName: "GitHub/GitLab",
+			color: "5",
+			percentage: "95",
+		},
+		{
+			title: "Version Control",
+			skillName: "JIRA",
+			color: "5",
+			percentage: "85",
+		},
+		{
+			title: "Version Control",
+			skillName: "Trello",
+			color: "5",
+			percentage: "80",
+		},
+	],
+	projects: {
+		web: [
+			{
+				projectName: "75 Hard",
+				image: "images/75hard.png",
+				summary:
+					"75 Hard is a mobile application, due to this app in 75 days you achieve your goal and develop your best character traits.",
+				preview: "https://apps.apple.com/app/75-hard/id1502228408",
+				techStack: [
+					"React",
+					"React-Bootstrap",
+					"GraphQL",
+					"Node",
+					"PostgreSQL",
+				],
+			},
+			{
+				projectName: "HSBC",
+				image: "images/hsbc.png",
+				summary:
+					"This Service is enabling businesses to thrive and economies to prosper, and, ultimately, helping people to fulfill their hopes and realize their ambitions.",
+				preview: "https://www.hsbc.com/",
+				techStack: ["React", "Styled-Components", "Node", "MongoDB", "Amazon S3"],
+			},
+			{
+				projectName: "Upside",
+				image: "images/upside.png",
+				summary:
+					"Web Portfolio to showcase acadmics, skills, projects and contact details in better manner.",
+				preview: "https://www.upside.com/",
+				techStack: ["React", "Node", "MongoDB", "Tailwind CSS", "Mocha/Chai"],
+			},
+			{
+				projectName: "AllTheRooms",
+				image: "images/alltheroom.png",
+				summary:
+					"AllTheRooms is a portal for booking rooms. This is an extensive search engine for travelers, combining the largest sites (Expedia, Airbnb, and Jetsetter) and other popular travel resources.",
+				preview: "http://alltherooms.com",
+				techStack: ["React", "Tailwind CSS", "Node", "MongoDB", "Mapbox", "Apexcharts.js"],
+			},
+			{
+				projectName: "E-CONCEPT",
+				image: "images/e-concept.png",
+				summary:
+					"This is a website to buy and sell real estate and offer a variety of services related to living so that customers can be satisfied even after the sale. ",
+				preview: "https://econcept.jp/",
+				techStack: ["PHP/WordPress", "MySQL", "JQuery", "FTP", "Google Font API"],
+			},
+		],
+	},
+	experience: [
+		{
+			title: "Senior Front-End Engineer",
+			duration: "Jan 2019 - May 2022",
+			subtitle: "Velog, Fully Remote Position",
+			details: [
+				"Developed new user-facing features using modern JS frameworks and libraries React.js, Angular and Vue",
+				"Completed plan, design, build and implement the user interface in almost 5 large projects",
+				"Worked with Redux to manage the global states and ReduxSaga to control the redux store",
+				"Built almost 100 reusable components and front-end libraries for future use",
+				"In 4+ projects, introduced the correct library and algorithm to reduce the page load time by 70 % ",
+				"Facilitated 8 ongoing projects and speed up by nearly 80% than before",
+				"Extensively used Git for version controlling and regularly pushed the code to GitHub.",
+				"Optimized components for 100% maximum performance across a vast array of web - capable devices and browsers",
+				"Collaborated together with an engineering team, a designer, and a project manager to achieve the goals of the application, then led to a 40 % increase in productivity",
+				"Improved the process that resulted in a 20hour time saving per week that saved $10K cost",
+				"Provided help and guidance to 4 junior developers on the project",
+				"Experienced leadership in 3 medium-size eCommerce projects leading the engineering team",
+				"Led and launched a big e-commerce project which received 20K unique hits and also 1000 paying clients in 2 weeks",
+
+			],
+			tags: [
+				"React",
+				"Angular",
+				"Vue",
+				"Node",
+				"GraphQL",
+				"MongoDB"
+			],
+			icon: "heartbeat",
+		},
+		{
+			title: "Full Stack Developer",
+			duration: "Feb 2015 - Dec 2018",
+			subtitle: "A4SAFE, Stafford",
+			details: [
+				"Implemented a 40+ satisfactory user experience with no issues,errors, or downtime",
+				"Created and optimized systems played an active role in testing and troubleshooting, and increased accessibility and browser compatibility by 40 % ",
+				"Served as an intermediary bridging the gap between the user and the backend developers, and tweaked 3 tools and platforms to adapt to real - world use situations",
+				"Organized 10+ high-resolution mock-ups to test interfaces with users",
+				"Finished 3 small projects alone using MERN or MEAN stack for faster cross - platform development",
+				"Built back-end APIs using Firebase cloud functions, a realtime database, and cloud messaging",
+				"Solved a payment integration problem that required high security and accuracy for different user banks.",
+				"Developed and published 8 Django/Spring projects including e - commerce, booking system, SaaS and mobile application",
+
+				"Collaborated with the design team of 25 to develop high quality software for Java / Spring Batch/ Microservice applications",
+				"Transformed monolithic app to microservices using Spring Boot via 12 - factor app methodology",
+				"Deployed, scaled, configured, and wrote manifest file for various Microservices in PCF",
+				"Oversaw centralized configuration & centralized logging by deploying Spring config server & Splunk, respectively",
+			],
+			tags: [
+				"React",
+				"Spring",
+				"Angular",
+				"Node",
+				"SaaS",
+			],
+			icon: "qrcode",
+		},
+		{
+			title: "Full Stack Developer",
+			duration: "Feb 2013 - Dec 2014",
+			subtitle: "CoreLogic, Los Angeles, Remote",
+			details: [
+				"Developed high-quality software design and architecture",
+				"Identified, prioritized, and executed tasks in the software development life cycle",
+				"Developed tools and applications by producing clean, efficient code",
+				"Automated tasks through appropriate tools and scripting",
+				"Reviewed and debug code",
+				"Performed validation and verification testing",
+				"Collaborated with internal teams and vendors to fix and improve products",
+				"Documented development phases and monitor systems",
+				"Ensured software is up-to-date with the latest technologies",
+
+			],
+			tags: ["JavaScript", "JQuery", "C#", "Bootstrap", "Java", "PHP"],
+			icon: "group",
+		},
+	],
+	education: [
+		{
+			title: "Bachelors in Computer Science",
+			duration: "2008 - 2012",
+			subtitle: "City University of Hong Kong",
+			details: [
+				"Computer Science",
+				"Software engineering",
+				"Artificial Intelligence & Mathematics and statistics.",
+				"Database System",
+				"GPA: 3.65"
+			],
+			tags: [
+			],
+			icon: "graduation-cap",
+		}
+	],
+	footer: [
+		// {
+		// 	label: "Dev Profiles",
+		// 	data: [
+		// 		{
+		// 			text: "Stackoverflow",
+		// 			link: "https://stackoverflow.com/users/8461233/vinay-somawat",
+		// 		},
+		// 		{
+		// 			text: "GitHub",
+		// 			link: "https://github.com/vinaysomawat",
+		// 		},
+		// 		{
+		// 			text: "LeetCode",
+		// 			link: "https://leetcode.com/somawatvinay/",
+		// 		},
+		// 	],
+		// },
+		// {
+		// 	label: "Resources",
+		// 	data: [
+		// 		{
+		// 			text: "Enable Dark/Light Mode",
+		// 			func: "enableDarkMode()",
+		// 		},
+		// 		{
+		// 			text: "Print this page",
+		// 			func: "window.print()",
+		// 		},
+		// 		{
+		// 			text: "Clone this page",
+		// 			link: "https://github.com/vinaysomawat/vinaysomawat.github.io",
+		// 		},
+		// 	],
+		// },
+		// {
+		// 	label: "Social Profiles",
+		// 	data: [
+		// 		{
+		// 			text: "Linkedin",
+		// 			link: "https://www.linkedin.com/in/vinaysomawat/",
+		// 		},
+		// 		{
+		// 			text: "Twitter",
+		// 			link: "https://twitter.com/thesigmakid",
+		// 		},
+		// 		{
+		// 			text: "Buy me a coffee",
+		// 			link: "https://www.buymeacoffee.com/r194dME8y",
+		// 		},
+		// 	],
+		// },
+		{
+			label: "copyright-text",
+			data: [
+				"Made with &hearts; by Lebron.",
+				"&copy; No Copyrights. Feel free to use this template."
+			],
+		},
+	],
+};
